15999fbc04ad35cc8d80dca61205f54ae36802b6
matteogirotti <matteogirotti10@gmail.com>
2024-09-21 21:36:56 +0200

    commit esercizio 2.1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    float a, b, media;
+    printf("inserisci il primo numero:3,14");
+    scanf("%f", &a);
+    printf("inserisci il secondo numero:10,333");
+    scanf("%f", &b);
+    media = (a+b) /2;
+    printf("la media è:");
+    printf("%f\n",media);
+    return 0;
+}
